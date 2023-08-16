615b04440f9c1372002ce9191d69dddbfc3bfa36
Simphiwe Mthanti <simphiwe@dots.africa>
2023-08-16 22:05:40 +0200

    Spring Boot products Microservice config server

---
+++ src/test/java/com/hopewell/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hopewell.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
